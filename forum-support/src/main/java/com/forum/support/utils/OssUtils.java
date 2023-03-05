4cb24b6ddb961f4cf14030f0f9845b1483139bf5
kafei-fish <2193998199@qq.com>
2023-03-05 15:15:12 +0800

    feature:
    create forum-support model

---
+++ forum-support/src/main/java/com/forum/support/utils/OssUtils.java
@@ -0,0 +1,17 @@
+package com.forum.support.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+/**
+ * @author lxl
+ * @description 对象存储（七牛云）
+ */
+public class OssUtils {
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        //upload();
+
+    }
+
+}
